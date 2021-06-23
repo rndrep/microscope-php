599bacb7854c04119184411ee3913f3862727dae
rndrep <hoshizone@gmail.com>
2021-06-24 02:53:19 +0700

    (Rock) Added model method for saving images, changed file path in config

--- app/Models/Rock.php
+++ app/Models/Rock.php
@@ -4,8 +4,53 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class Rock extends Model
 {
     use HasFactory;
+
+    const IMAGE_PATH_ROCK_INFO = 'images/rocks/detail/';
+    const IMAGE_PATH_ROCK_MICRO = 'images/rocks/microscope/';
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    public function formingMineral()
+    {
+        $this->belongsTo(Mineral::class, 'forming_mineral_id');
+
+    }
+
+    public function accessoryMineral()
+    {
+        $this->belongsTo(Mineral::class, 'accessory_mineral_id');
+
+    }
+
+    public function rockType()
+    {
+        $this->belongsTo(RockType::class);
+    }
+
+    public function uploadImage(UploadedFile $image): bool
+    {
+        if (empty($image)) {
+            return false;
+        }
+
+        // TODO: check that save and remove in correct path
+        Storage::delete($this::IMAGE_PATH_ROCK_INFO . $this->photo);
+        $filename = $this->getKey() . '.' . $image->extension();
+        $image->storeAs($this::IMAGE_PATH_ROCK_INFO, $filename);
+        $this->photo = $filename;
+        $this->save();
+        return true;
+    }
+
 }
