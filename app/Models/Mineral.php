8742095a06fbbf13cb3ab60d2f3a2c61c5071d78
rndrep <hoshizone@gmail.com>
2021-11-06 15:51:52 +0700

    Replaced magick getters and setters with simple methods, because other methods are broken

--- app/Models/Mineral.php
+++ app/Models/Mineral.php
@@ -3,57 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
+//use Illuminate\Support\Str;
 
 /**
  * Each Rock can have several forming, second and accessory minerals
  *
  * Class Mineral
  * @package App\Models
- *
- * @method getName()
- * @method setName($value)
- * @method getDescription()
- * @method setDescription($value)
- * @method getComposition()
- * @method setComposition($value)
- * @method getClass()
- * @method setClass($value)
- * @method getPhoto()
- * @method setPhoto($value)
- * @method getVideo()
- * @method setVideo($value)
- * @method getVarieties()
- * @method setVarieties()
- * @method getAggregates()
- * @method setAggregates()
- * @method getFeature()
- * @method setFeature()
- * @method getSyngony()
- * @method setSyngony()
- * @method getCrystalForm()
- * @method setCrystalForm()
- * @method getHardness()
- * @method setHardness()
- * @method getSpecificGravity()
- * @method setSpecificGravity()
- * @method getColor()
- * @method setColor()
- * @method getFeatureColor()
- * @method setFeatureColor()
- * @method getShine()
- * @method setShine()
- * @method getTransparency()
- * @method setTransparency()
- * @method getOtherProps()
- * @method setOtherProps()
- * @method getDiagnosis()
- * @method setDiagnosis()
- * @method getGenesis()
- * @method setGenesis()
- * @method getParagenesis()
- * @method setParagenesis()
  */
 class Mineral extends Model
 {
@@ -66,19 +22,29 @@ class Mineral extends Model
      */
     protected $guarded = [];
 
-    public function __call($name, $arguments)
+    public function __construct(array $attributes = [])
     {
-        //do a get
-        if (preg_match('/^get(.+)/', $name, $matches)) {
-            $varName = Str::snake($matches[1]);
-            return $this->$varName;
-        }
-        //do a set
-        if (preg_match('/^set(.+)/', $name, $matches)) {
-            $varName = Str::snake($matches[1]);
-            $this->$varName = $arguments[0];
-        }
+        parent::__construct($attributes);
+        $this->imagePathDetail = '/images/minerals/detail/';
+        $this->imagePathGallery = '/images/minerals/gallery/';
     }
+
+//doesn't work with Mineral::pluck
+//TODO: need to make getters ans setters
+//    public function __call($name, $arguments)
+//    {
+//        //do a get
+//        if (preg_match('/^get(.+)/', $name, $matches)) {
+//            $varName = Str::snake($matches[1]);
+//            return $this->$varName;
+//        }
+//        //do a set
+//        if (preg_match('/^set(.+)/', $name, $matches)) {
+//            $varName = Str::snake($matches[1]);
+//            $this->$varName = $arguments[0];
+//        }
+////        call_user_func($name, $arguments);
+//    }
 
     public static function getOptionalProps()
     {
@@ -121,6 +87,237 @@ class Mineral extends Model
         $point->x = $this->x;
         $point->y = $this->y;
         return $point;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($value): self
+    {
+        $this->name = $value;
+        return $this;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($value): self
+    {
+        $this->description = $value;
+        return $this;
+    }
+
+    public function getComposition()
+    {
+        return $this->composition;
+    }
+
+    public function setComposition($value): self
+    {
+        $this->composition = $value;
+        return $this;
+    }
+
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+    public function setClass($value): self
+    {
+        $this->class = $value;
+        return $this;
+    }
+
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto($value): self
+    {
+        $this->photo = $value;
+        return $this;
+    }
+
+    public function getVideo()
+    {
+        return $this->video;
+    }
+
+    public function setVideo($value): self
+    {
+        $this->video = $value;
+        return $this;
+    }
+
+    public function getVarieties()
+    {
+        return $this->varieties;
+    }
+
+    public function setVarieties($value): self
+    {
+        $this->varieties = $value;
+        return $this;
+    }
+
+    public function getAggregates()
+    {
+        return $this->aggregates;
+    }
+
+    public function setAggregates($value): self
+    {
+        $this->aggregates = $value;
+        return $this;
+    }
+
+    public function getFeature()
+    {
+        return $this->feature;
+    }
+
+    public function setFeature($value): self
+    {
+        $this->feature = $value;
+        return $this;
+    }
+
+    public function getSyngony()
+    {
+        return $this->syngony;
+    }
+
+    public function setSyngony($value): self
+    {
+        $this->syngony = $value;
+        return $this;
+    }
+
+    public function getCrystalForm()
+    {
+        return $this->crystal_form;
+    }
+
+    public function setCrystalForm($value): self
+    {
+        $this->crystal_form = $value;
+        return $this;
+    }
+
+    public function getHardness()
+    {
+        return $this->hardness;
+    }
+
+    public function setHardness($value): self
+    {
+        $this->hardness = $value;
+        return $this;
+    }
+
+    public function getSpecificGravity()
+    {
+        return $this->specific_gravity;
+    }
+
+    public function setSpecificGravity($value): self
+    {
+        $this->specific_gravity = $value;
+        return $this;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setColor($value): self
+    {
+        $this->color = $value;
+        return $this;
+    }
+
+    public function getFeatureColor()
+    {
+        return $this->feature_color;
+    }
+
+    public function setFeatureColor($value): self
+    {
+        $this->feature_color = $value;
+        return $this;
+    }
+
+    public function getShine()
+    {
+        return $this->shine;
+    }
+
+    public function setShine($value): self
+    {
+        $this->shine = $value;
+        return $this;
+    }
+
+    public function getTransparency()
+    {
+        return $this->transparency;
+    }
+
+    public function setTransparency($value): self
+    {
+        $this->transparency = $value;
+        return $this;
+    }
+
+    public function getOtherProps()
+    {
+        return $this->other_props;
+    }
+
+    public function setOtherProps($value): self
+    {
+        $this->other_props = $value;
+        return $this;
+    }
+
+    public function getDiagnosis()
+    {
+        return $this->diagnosis;
+    }
+
+    public function setDiagnosis($value): self
+    {
+        $this->diagnosis = $value;
+        return $this;
+    }
+
+    public function getGenesis()
+    {
+        return $this->genesis;
+    }
+
+    public function setGenesis($value): self
+    {
+        $this->genesis = $value;
+        return $this;
+    }
+
+    public function getParagenesis()
+    {
+        return $this->paragenesis;
+    }
+
+    public function setParagenesis($value): self
+    {
+        $this->paragenesis = $value;
+        return $this;
     }
 
 }
