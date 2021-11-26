cdae2e07eecc139b4bac270892bd2bff9e91895d
Your Name <hoshizone@gmail.com>
2021-11-26 15:09:21 +0700

    created get url for microscope

--- frontend/src/assets/js/app.js
+++ frontend/src/assets/js/app.js
@@ -2,25 +2,20 @@
 
 import $ from "jquery";
 import "bootstrap";
-
 import map from "./modules/map";
-import microscope from "./modules/microscope";
 import slick from "./modules/slick";
 import select from "./modules/select";
 
 window.addEventListener("DOMContentLoaded", () => {
-	// slick-carousel
-	$("#accordion-button-gallery").on("click", function (event) {
-		slick();
-	});
+    // slick-carousel
+    $("#accordion-button-gallery").on("click", function (event) {
+        slick();
+    });
 
-	// leaflet map
-	$("#accordion-button-map").on("click", function (event) {
-		map();
-	});
+    // leaflet map
+    $("#accordion-button-map").on("click", function (event) {
+        map();
+    });
 
-	select();
-	microscope("http://vmicro.tpu.ru/microscope/rock/test");
+    select();
 });
-
-export default microscope;
