19680005cf6687a9eca2e6dbc63d0e926d6d14ec
Your Name <hoshizone@gmail.com>
2021-11-27 15:53:06 +0700

    Integrate Laravel with AdminLTE

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-window._ = require('lodash');
+window._ = require("lodash");
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6,9 +6,16 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+try {
+    window.$ = window.jQuery = require("jquery").default;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    require("bootstrap");
+    require("admin-lte");
+} catch (e) {}
+
+window.axios = require("axios");
+
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
